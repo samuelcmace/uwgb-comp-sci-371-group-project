776ee4c09a3f1f1ecdd99e8ecafde25d45813441
Derek-Waddick <derekwaddick1@gmail.com>
2024-12-06 20:51:12 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -16,7 +16,11 @@ int main(int argc, char* argv[])
     UserManager userManager = UserManager::getInstance();
     //Main loop of the program
     //Initial input:
-    int input = 0;
+    double amount;
+    bool login;
+    int input;
+    int managerInput;
+    User user1;
     while (input != 4)
     {
         input = 0;
@@ -25,7 +29,7 @@ int main(int argc, char* argv[])
         if (input == 1)
         {
             std::string username, password;
-            bool login = false;
+            login = false;
             while (login == false)
             {
                 //User Login Selected
@@ -45,6 +49,13 @@ int main(int argc, char* argv[])
             {
 
             }
+            user1 = userManager.getUserByUsername(username);
+            //TODO: check if user is a manager/customer
+            //if (user1 is manager/employee) {
+            // cout << "You are a manager. Login to the manager login.";
+            // continue;
+            //}
+            
             //User Options:
             int userOptions = 0;
             while (userOptions != 4)
@@ -55,20 +66,33 @@ int main(int argc, char* argv[])
                 if (userOptions == 1)
                 {
                     //Withdrawal Selected
-                    //TODO
-                    //verify availability of funds, deduct specified amount if funds are available
+                    amount = 0.0;
+                    cout << "Amount for withdrawal: ";
+                    cin >> amount;
+                    if (user1.withdraw(amount))
+                    {
+                        cout << "Withdrawal Successful." << endl;
+                    }
+                    else
+                    {
+                        continue;
+                    }
                 }
                 else if (userOptions == 2)
                 {
                     //Deposit
-                    //TODO
-                    //add money to account
+                    amount = 0.0;
+                    cout << "Amount for deposit: ";
+                    cin >> amount;
+                    if (user1.deposit(amount))
+                    {
+                        cout << "Deposit Successful." << endl;
+                    }
                 }
                 else if (userOptions == 3)
                 {
                     //Print Account Summary
-                    //TODO
-                    //transaction history and current account balance
+                    user1.printAccountSummary();
                 }
                 else
                 {
@@ -76,18 +100,69 @@ int main(int argc, char* argv[])
                     continue;
                 }
             }
-            //delete user
         }
         else if (input == 2)
         {
             //Create Account
-            //TODO
+            string usernam, passwor;
+            cout << "Creating new account.\nEnter Username: ";
+            cin >> usernam;
+            cout << "Enter Password: ";
+            cin >> passwor;
+            userManager.createUser(usernam, passwor, CUSTOMER);
+            cout << "Account successfully created." << endl;
         }
         else if (input == 3)
         {
             //Manager Login
             //TODO
             //Authenticate Manager login, give access to creating/deleting/managing accounts.
+            string userN, passW;
+            cout << "Manager Login\nEnter Username: ";
+            cin >> userN;
+            cout << "Enter Password: ";
+            cin >> passW;
+            if (userManager.authenticateUser(userN, passW))
+            {
+                user1 = userManager.getUserByUsername(userN);
+            }
+            else
+            {
+                continue;
+            }
+            managerInput = 0;
+            while (managerInput != -1)
+            {
+                cout << "Manager Options:\n1. Create Account\n2. Delete Account\n3. Edit Balance\n4. Logout\n";
+                if (managerInput == 1)
+                {
+                    //Create Account
+                    string userna, passwo;
+                    cout << "Creating new account.\nEnter Username: ";
+                    cin >> userna;
+                    cout << "Enter Password: ";
+                    cin >> passwo;
+                    userManager.createUser(userna, passwo, CUSTOMER);
+                    cout << "Account successfully created." << endl;
+                }
+                else if (managerInput == 2)
+                {
+                    //Delete Account
+                    string enteredUsername;
+                    cout << "Enter username of account to be deleted:";
+                    cin >> enteredUsername;
+                    //TODO: delete method of UserManager
+                }
+                else if (managerInput == 3)
+                {
+                    //Edit Balance
+                    //TODO: function in class Customer to edit balance.
+                }
+                else
+                {
+                    break;
+                }
+            }
         }
         else
         {
@@ -96,8 +171,6 @@ int main(int argc, char* argv[])
         }
     }
     std::cout << "\nExiting Program...";
-
-    CSVObject csv;
 
     return 0;
 
